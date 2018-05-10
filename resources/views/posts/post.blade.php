51d3c39c73b8fd723ce99570cc586a1ec6967686
vemarun <vemarun@gmail.com>
2018-05-10 22:56:12 +0530

    updated dependencies

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -5,5 +5,5 @@
 <i class="fas fa-user text-muted"></i> &nbsp;<a href="#"> {{$post->user->name}}</a> on&nbsp; <i class="fas fa-calendar-alt text-muted"></i> &nbsp;{{$post->created_at->toFormattedDateString() }}</p>
 
 <p> {!! strip_tags(str_limit($post->body,400)) !!}</p>
-<a href="posts/{{$post->id}}" class="btn btn-primary mt-3">Read More <span class="fas fa-arrow-right"></span></a>
+<a href="posts/{{$post->id}}" class=" mt-3">Read More <span class="fas fa-arrow-right"></span></a>
 <hr>
