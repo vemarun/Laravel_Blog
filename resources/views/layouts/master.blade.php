82b1e211792f486f8832e78090fcf3b38b9fef6f
vemarun <vemarun@gmail.com>
2018-05-17 16:37:55 +0530

    added create post link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,9 +22,9 @@
 	
 	<div style="color:#A4A4A4;font-weight:600;letter-spacing:2px">
 	@if(Auth::check())
-		<span class="d-md-inline float-left">Welcome, <a href="/logout">{{Auth::user()->name}}</a></span>
+		<span class="d-md-inline float-left">Welcome, <a href="/posts/create">{{Auth::user()->name}}</a></span>
 	@else
-	   <span class="d-md-inline float-left">Welcome, Guest <a href="/posts/create">Login</a></span>
+	   <span class="d-md-inline float-left">Welcome, Guest <a href="/login">Login</a></span>
 	@endif
 	
 		<span style="float:right" class="d-none d-sm-block col-centered pt-1">
